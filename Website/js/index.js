d97acd2cc8cd0cab28da975addca1e270b0926ff
iAnomaly27 <ianomaly27@gmail.com>
2016-11-07 20:23:24 -0600

    Add files via upload

---
+++ Website/js/index.js
@@ -0,0 +1,11 @@
+WebFontConfig = {
+    google: { families: [ 'Lato:400,300,300italic,400italic,700,700italic,900,900italic:latin' ] }
+  };
+  (function() {
+    var wf = document.createElement('script');
+    wf.src = 'https://ajax.googleapis.com/ajax/libs/webfont/1/webfont.js';
+    wf.type = 'text/javascript';
+    wf.async = 'true';
+    var s = document.getElementsByTagName('script')[0];
+    s.parentNode.insertBefore(wf, s);
+  })();
